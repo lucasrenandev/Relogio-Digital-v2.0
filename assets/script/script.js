3fa90590e27cf30202eb5ff8339853f2d7c9ca80
Lucas Renan <lucasrenandns96@gmail.com>
2022-08-16 17:46:02 -0300

    Relógio Digital

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+
+const container = document.querySelector(".container")
+const horas = container.querySelector("#horas")
+const minutos = container.querySelector("#minutos")
+const segundos = container.querySelector("#segundos")
+
+function Number(element) {
+    if(element < 10) {
+        return '0' + element
+    }
+    else {
+        return element
+    }
+}
+
+const time = setInterval(function () {
+
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    horas.textContent = Number(hours)
+    minutos.textContent = Number(minutes)
+    segundos.textContent = Number(seconds)
+})
